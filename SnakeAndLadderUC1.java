2e9324b2ba0d67099daafe084736f2ffc77740f6
rohitmartand123 <rohitmartand274@gmail.com>
2022-06-22 18:44:32 +0530

    Added: Program Snake and ladder game played with single player at sta…

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,14 @@
+package assignmentday4snakeandladder;
+/*
+ * Author: Rohit
+ * Snake and ladder game played with single player at start position 0
+ */
+public class SnakeAndLadderUC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to snake and ladder game");
+		String p1 = "Player1";
+		int position = 0;
+		System.out.println("Player starts from Position :" + position);
+	}
+}
